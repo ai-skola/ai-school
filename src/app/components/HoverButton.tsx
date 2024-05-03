a64f58420acbdb290b74b37ee1a7928adc6c9405
sabashatirishvili <shatirishvilis1@gmail.com>
2024-05-03 16:55:56 +0400

    Initial Commit

---
+++ src/app/components/HoverButton.tsx
@@ -0,0 +1,9 @@
+// import React from 'react'
+
+// export default function HoverButton() {
+//   return (
+//     <div>
+//       {children}
+//     </div>
+//   )
+// }
---
+++ src/app/components/Menu.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Menu() {
+  return (
+    <div>
+      <div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+      <div></div>
+    </div>
+  )
+}
